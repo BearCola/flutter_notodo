d850f9b17ce1e12ea43250507fc6fb8cd4c6a249
Sangin <sangin.kang@navercorp.com>
2019-06-20 08:40:57 +0900

    first commit

---
+++ lib/model/nodo_item.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class NoDoItem extends StatelessWidget {
+  String _itemName;
+  String _dateCreated;
+  int _id;
+
+  NoDoItem(this._itemName, this._dateCreated);
+
+  NoDoItem.map(dynamic obj) {
+    this._itemName = obj['itemName'];
+    this._dateCreated = obj['dateCreated'];
+    this._id = obj['id'];
+  }
+
+  String get itemName => _itemName;
+  String get dateCreated => _dateCreated;
+  int get id => _id;
+
+  Map<String, dynamic> toMap() {
+    var map = new Map<String, dynamic>();
+    map['itemName'] = _itemName;
+    map['dateCreated'] = _dateCreated;
+
+    if (_id != null) {
+      map['id'] = _id;
+    }
+
+    return map;
+  }
+
+  NoDoItem.fromMap(Map<String, dynamic> map) {
+    this._itemName;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
